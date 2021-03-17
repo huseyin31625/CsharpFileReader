e7cd7d5f19192caf3ac0bcca40d62db46185de77
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2021-03-17 14:58:56 +0300

    Add files via upload

---
+++ CodeDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CsharpFileReader
+{
+    public partial class CodeDialog : Form
+    {
+        public CodeDialog()
+        {
+            InitializeComponent();
+        }
+        private void ShowMessageDialog(string code,string filename)
+        {
+            textBox1.Text = code;
+            this.Text = filename;
+        }
+        internal virtual void ShowCode(string code,string filename)
+        {
+            ShowMessageDialog(code,filename);
+            this.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CsharpFileReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string ss in openFileDialog2.FileNames)
+                {
+                    new CodeDialog().ShowCode(File.ReadAllText(ss),ss);
+                }
+            }
+        }
+    }
+}
